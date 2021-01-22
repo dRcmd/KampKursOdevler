96dcdde3f766fc40db6ef5ff104d34eef387a7e2
dRcmd <76197823+dRcmd@users.noreply.github.com>
2021-01-22 22:55:42 +0300

    update

---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kapı açık(true)-kapalı(false) listesi
+            Dictionary<string, bool> doors = new Dictionary<string, bool>();
+            
+            doors.Add("door1",true);
+            doors.Add("door2",false);
+            doors.Add("door3", true);
+            doors.Add("door4", true);
+            doors.Add("door5", false);
+            doors.Add("door6", true);
+
+            foreach (var door in doors)
+            {
+                Console.WriteLine(door);
+            }
+
+        }
+    }
+}
